a15ca9c356f6fc348b9403a52d651894e7ba5ac3
Felipe Miranda <felipe.fdmiranda@gmail.com>
2024-02-08 23:38:18 -0300

    teste

---
+++ src/domain/repository/customer-repository-interface.ts
@@ -0,0 +1,6 @@
+import RepositoryInterface from "./repository-interface";
+import Customer from "../entity/custumer";
+
+export default interface CustomerRepositoryInterface extends RepositoryInterface<Customer> {
+
+}
